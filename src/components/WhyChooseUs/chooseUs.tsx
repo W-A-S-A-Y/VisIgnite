4afc55d1675841f5e99dc691742450acc3d8364e
W-A-S-A-Y <65955504+W-A-S-A-Y@users.noreply.github.com>
2024-09-24 02:31:43 +0500

    commit pushes

--- src/components/WhyChooseUs/chooseUs.tsx
+++ src/components/WhyChooseUs/chooseUs.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-
+import SectionTitle from '../Common/SectionTitle';
 const WhyChooseUs: React.FC = () => {
   return (
+  <div>
+    <SectionTitle title="Why Choose Us" paragraph='' center  />  
     <div style={styles.wrapper}>
       <div style={styles.container}>
         <div style={styles.section}>
-          <h3>Why Choose Us</h3>
+          <h3 style={{ fontWeight: 'bold', fontSize: '18px' }}>Why Choose Us</h3>
           <p>
-            At Savvital, we offer a blend of professionalism, versatility, and dynamism,
+            At VisIgnite, we offer a blend of professionalism, versatility, and dynamism,
             tailoring virtual services to meet your unique business needs.
           </p>
         </div>
         <div style={styles.section}>
-          <h3>Save Your Time</h3>
+            <h3 style={{ fontWeight: 'bold', fontSize: '18px' }}>Save Your Time</h3>
           <p>
             Time is money in business, and Savvital saves you both. Our team handles your operations,
             freeing you to focus on growth and profit.
           </p>
         </div>
         <div style={styles.sectionHighlight}>
-          <h3>Cost Effective</h3>
+          <h3 style={{ fontWeight: 'bold', fontSize: '18px' }}>Cost Effective</h3>
           <p>
             We deliver cost-effectiveness by offering highly skilled services at competitive rates,
             ensuring you get the best ROI for your business.
           </p>
         </div>
         <div style={styles.section}>
-          <h3>Tailored Service</h3>
+          <h3 style={{ fontWeight: 'bold', fontSize: '18px' }}>Tailored Service</h3>
           <p>
             Personalized, flexible virtual assistance. We tailor our services to your unique business
             needs, providing the right support when and how you need it.
@@ -34,6 +36,7 @@ const WhyChooseUs: React.FC = () => {
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
@@ -55,7 +58,7 @@ const styles = {
   section: {
     flex: 1,
     padding: '20px',
-    backgroundColor: '#FFC107',   // Standard orange background
+    backgroundColor: '#86a7e0',   // Standard orange background
     borderRadius: '10px',
     margin: '0 10px',             // Space between the sections
     textAlign: 'center' as const,
@@ -69,7 +72,7 @@ const styles = {
   sectionHighlight: {
     flex: 1.2,                      // Slightly larger box
     padding: '20px',
-    backgroundColor: '#004D40',     // Dark green background
+    backgroundColor: '#324898',     // Dark green background
     borderRadius: '10px',
     margin: '0 10px',               // Same margin to maintain spacing
     textAlign: 'center' as const,
