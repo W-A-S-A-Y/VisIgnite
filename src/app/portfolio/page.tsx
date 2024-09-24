ecdeb8266cd9a3ff9a72b4edff16c10151f8d4ad
W-A-S-A-Y <65955504+W-A-S-A-Y@users.noreply.github.com>
2024-09-24 15:04:16 +0500

    commite

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from "next";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import PortfolioGrid from "@/components/portfolio/portfolioGrid";
-
+import Carousel from "@/components/suii/suii";
 
 export const metadata: Metadata = {
   title: "Our Portfolio",
@@ -17,7 +17,7 @@ const Portfolio = () => {
         pageName="Portfolio"
         description="Here's a detail on our past work."
       />
-
+        <Carousel/>
         <PortfolioGrid />
     </>
   );
